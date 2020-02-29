40f4e20869a5592b5b8296062e377bdb5b3de9b4
laimaster <cyuan42@wisc.edu>
2020-02-29 21:25:29 +0000

    working on m1

--- app/src/main/java/c/sakshi/lab5/MainActivity.java
+++ app/src/main/java/c/sakshi/lab5/MainActivity.java
@@ -1,10 +1,27 @@
 package c.sakshi.lab5;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    public void clickFunction1( View view){
+        Log.i("info","button1 pressed");
+        EditText myTextField=(EditText) findViewById(R.id.editText);
+        //Toast.makeText(MainActivity.this,myTextField.getText().toString(),Toast.LENGTH_LONG).show();
+        String str=myTextField.getText().toString();
+        gotoActivity2(str);
+    }
 
+    public void gotoActivity2(String s){
+        Intent intent=new Intent(this,Main2Activity.class);
+        intent.putExtra("dollars",s);
+        startActivity(intent);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
